65c65bf4482eadf40b94308af1e4eae50816fd28
Platypus <jennifersalexander@gmail.com>
2022-09-15 23:30:55 -0700

    Got the divs to show up in the HTML! Tomorrow work on adding the classes, etc. to make the div match the div you created in HTML.

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,18 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
-var hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var container = document.querySelector(".container");
+var hoursInTheDay = 8;
 
-var container = document.getElementById("container");
+function createSchedule(container2) {
+  var n = $("<div>").addClass("input-group mb-3").appendTo(container);
+  var text = "Hello World!";
+  var timeBlockDiv;  
 
+  for(var i = 0; i < hoursInTheDay; i++) {
+    timeBlockDiv = $("<div>").addClass("block").appendTo(container);
+    $('<span>').text(text).appendTo(timeBlockDiv);
+  }
+}
 
-    for (var i = 0; i <= 8; i++) {
-      document.write('<div>' + i.toString() + '</div>');
-    }
-
-
-    <div class="input-group mb-3"><span class="input-group-text" id="01">01pm</span>
-    <textarea class="col-md-10 time-block" placeholder="Add your event"></textarea>
-    <button class="col-md-1 saveBtn" aria-label="save button 9AM"><i class='fa fa-save'></i></button>
-  </div>
+createSchedule();
