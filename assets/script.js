af92fd2775951d35778b9e6a250ab6bb28298fd3
Platypus <jennifersalexander@gmail.com>
2022-09-19 14:13:07 -0700

    Worked with tutor today. Decided to abandon plans for adding HTML through JS and just hardcoded it. Added my save and recall features and started working on the color coding. The code looks right, but it's not showing up correctly. Need to take another look at the CSS.

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,76 @@
-var container = document.querySelector(".container");
-var hours = document.querySelectorAll(".hours");
-var textAreaEl = document.getElementById(".col-md-10");
-var hours = document.querySelectorAll("#hours");
-
-var k = document.querySelectorAll(".k");
-
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
-let timeBlockHtmlString = [""];
-let hoursArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-console.log(hoursArray);
 
-function createSchedule() {
+function blockColors() {
+  var hour = moment().hours();
 
-   for ( let i = 0; i < 8; i++) {
-    timeBlockHtmlString += `<div class="input-group mb-3"><span class="input-group-text" id="hours"></span>
-    <textarea class="col-md-10 time-block" placeholder="Add your event"></textarea>
-     <button class="col-md-1 saveBtn" aria-label="save button"><i class='fa fa-save'></i></button>
-     </div>`; 
-    container.innerHTML = timeBlockHtmlString;
-}
-}
-createSchedule();
+  $(".timeSquare").each(function() {
+      var currentHour = parseInt($(this).attr("id"));
+
+          if (currentHour > hour) {
+          $(this).addClass("future");
+      } else if (currentHour === hour) {
+          $(this).addClass("present");
+          $(this).removeClass("future");
+      } else {
+          $(this).addClass("past");
+          $(this).removeClass("present");
+          $(this).removeClass("future");
+      }
+  })
+};
+
+blockColors();
+
+$(".saveBtn").on("click", function() {
+var value = $(this)
+.siblings(".time-block")
+.val()
+var time = $(this)
+.parent()
+.attr("id")
+localStorage.setItem(time, value);
+})
+
+$("#9 .time-block").val(localStorage.getItem("9"));
+$("#10 .time-block").val(localStorage.getItem("10"));
+$("#11 .time-block").val(localStorage.getItem("11"));
+$("#12 .time-block").val(localStorage.getItem("12"));
+$("#13 .time-block").val(localStorage.getItem("13"));
+$("#14 .time-block").val(localStorage.getItem("14"));
+$("#15 .time-block").val(localStorage.getItem("15"));
+$("#16 .time-block").val(localStorage.getItem("16"));
+$("#17 .time-block").val(localStorage.getItem("17"));
 
 
 
+
+
+
+
+
+
+
+// let timeBlockHtmlString = [""];
+// let hoursArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+// console.log(hoursArray);
+
+// function createSchedule() {
+
+//    for ( let i = 0; i < 8; i++) {
+//     timeBlockHtmlString += `<div class="input-group mb-3"><span class="input-group-text" id="hours"></span>
+//     <textarea class="col-md-10 time-block" placeholder="Add your event"></textarea>
+//      <button class="col-md-1 saveBtn" aria-label="save button"><i class='fa fa-save'></i></button>
+//      </div>`; 
+//     container.innerHTML = timeBlockHtmlString;
+// }
+// }
+// createSchedule();
+
+
+
+// $(".input-group-text").each(function (index) { $(this).siblings(".input-group-text").text(hoursArray[index]); });
   
 
 
